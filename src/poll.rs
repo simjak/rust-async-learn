e719a83a8153653b8a821206f88cbc0a7de52419
Simonas <20096648+simjak@users.noreply.github.com>
2024-04-15 10:05:10 +0300

    feat: Poll setup

--- src/poll.rs
+++ src/poll.rs
@@ -8,14 +8,28 @@ pub struct Poll {
 
 impl Poll {
     pub fn new() -> Result<Self> {
-        todo!()
+        let res = unsafe { ffi::epoll_create(1)};
+        if res < 0 {
+            return Err(io::Error::last_os_error());
+        }
+        Ok(Self {
+            registry: Registry {raw_fd: res},
+        })
     }
     pub fn registry(&self) -> &Registry {
         &self.registry
     }
     pub fn poll(&mut self, events: &mut Events, timeout: Option<i32>) -> Result<()>
     {
-        todo!()
+        let fd = self.registry.raw_fd;
+        let timeout = timeout.unwrap_or(-1);
+        let max_events = events.capacity() as i32;
+        let res = unsafe {ffi::epoll_wait(fd, events.as_mut_ptr(), max_events, timeout)};
+        if res < 0 {
+            return Err(io::Error::last_os_error());
+        };
+        unsafe { events.set_len(res as usize)};
+        Ok(())
     }
 }
 
@@ -26,12 +40,27 @@ pub struct Registry {
 impl Registry {
     pub fn register(&self, source: &TcpStream, token: usize, interests: i32) -> Result<()>
     {
-        todo!()
+        let mut event = ffi::Event {
+            events: interests as u32,
+            epoll_data: token,
+        };
+        let op = ffi::EPOLL_CTL_ADD;
+        let res = unsafe {
+            ffi::epoll_ctl(self.raw_fd, op, source.as_raw_fd(), &mut event)
+        };
+        if res < 0 {
+            return Err(io::Error::last_os_error());
+        }
+        Ok(())
     }
 }
 
 impl Drop for Registry {
     fn drop (&mut self) {
-        todo!()
+        let res = unsafe { ffi::close(self.raw_fd)};
+        if res < 0 {
+            let err = io::Error::last_os_error();
+            eprintln!("ERROR: {err:?}");
+        }
     }
 }
