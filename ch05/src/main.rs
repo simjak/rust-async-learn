a9306221532cca31097d0ded5d11bab88245ab0a
simjak <ubuntu@ip-10-0-53-12.eu-north-1.compute.internal>
2024-04-19 07:01:15 +0000

    feat: show stack

--- ch05/src/main.rs
+++ ch05/src/main.rs
@@ -1,5 +1,6 @@
 use core::arch::asm;
 
+
 const SSIZE: isize = 48;
 
 #[derive (Debug, Default)]
@@ -29,7 +30,13 @@ fn main() {
         let sb_aligned = (stack_bottom as usize & !15) as *mut u8;
         std::ptr::write(sb_aligned.offset(-16) as *mut u64, hello as u64);
         ctx.rsp = sb_aligned.offset(-16) as u64;
-        gt_switch(&mut ctx);
+        // gt_switch(&mut ctx);
+
+        for i in 0..SSIZE {
+            println!("mem: {}, val: {}, pointer {}", sb_aligned.offset(-i as isize) as usize, *sb_aligned.offset(-i as isize));
+
+        }
     }
+
 }
 
---
+++ ch05/target/debug/ch05.d
@@ -0,0 +1,1 @@
+/home/ubuntu/rust-async-learn/ch05/target/debug/ch05: /home/ubuntu/rust-async-learn/ch05/src/main.rs
